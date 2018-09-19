b39399cc1198ac8c55e98777e359c4765e6120a3
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-09-19 19:44:06 +0700

    Registration Form

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: boonkuaeboonsutta
+ * Date: 19/9/2018 AD
+ * Time: 19:22
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserRegistrationForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function registerAction(Request $request)
+    {
+        $form = $this->createForm(UserRegistrationForm::class);
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            /** @var User $user */
+            $user = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success','Welcome '.$user->getEmail());
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render(
+            'user/register.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+            );
+    }
+}
